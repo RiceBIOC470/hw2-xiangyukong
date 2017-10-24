cdae56f58406914e63411addcde2bf388622b7c0
gbri140 <glb3@rice.edu>
2017-10-24 13:14:27 -0500

    Update HW2.m

--- HW2.m
+++ HW2.m
@@ -1,3 +1,17 @@
+% GB comments:
+1a. 100
+1b. 0 No solution provided
+2a. 100
+2b. 100
+2c. 100
+2d. 100
+2e. 100
+3a.  100
+3b. 100 
+3c.  100
+Overall: 90
+
+
 %% Problem 1. 
 % in the repository you will find the meannonan.m function we discussed in
 % class which produced the mean of a vector of numbers that ignores values
